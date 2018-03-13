945bdbaa63113ab2b1501e79f42f51306af9f639
Milt Reder <milt@yetanalytics.com>
2018-03-13 17:52:17 -0400

    removed test requires until we have tests `

--- test/com/yetanalytics/sse_fx_test.clj
+++ test/com/yetanalytics/sse_fx_test.clj
@@ -1,6 +1,5 @@
 (ns com.yetanalytics.sse-fx-test
-  (:require [clojure.test :refer :all]
-            [com.yetanalytics.sse-fx :refer :all]))
+  (:require [clojure.test :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
